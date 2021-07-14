249991645647b94dcb4092be9902fc7cd4bdbf2e
jay <jaylong255@gmail.com>
2021-07-14 13:41:54 -0500

    installing sanctum for auth

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
