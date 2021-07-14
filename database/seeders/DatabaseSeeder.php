d8078ab54fafeff16f05c45e3f92623c4aeb124d
jay <jaylong255@gmail.com>
2021-07-14 13:32:42 -0500

    seeding myself as a user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $jay = User::create([
+            'name' =>    "Jay",
+            'email' =>  "jaylong255@gmail.com",
+            'password'  =>  Hash::make('Test123$')
+        ]);
+
     }
 }
